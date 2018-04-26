801b0fbd54e383087f43cab91621c08f97ac9d31
Ethan Elliott <ethaelli696@ed.amdsb.ca>
2018-04-26 13:00:33 -0400

    Add project files.

---
+++ DrawTriangle/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawTriangle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        public void drawTriangle(int x1, int x2, int y1, int y2, int x3, int y3, Pen pen)
+        {
+            //draws a triangle
+            Graphics g = this.CreateGraphics();
+            g.DrawLine(pen, x1, y1, x2, y2);
+            g.DrawLine(pen, x2, y2, x3, y3);
+            g.DrawLine(pen, x3, y3, x1, y1);
+        }
+    }
+}
